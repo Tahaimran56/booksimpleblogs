3ebaf751245862a347f2b5bcf7c11a7c5b023089
Taha Qureshi <160122852+Tahaimran56@users.noreply.github.com>
2024-12-29 04:30:36 -0800

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -65,7 +65,7 @@ export default   function ContactUs(){
             </div>
             <div className="p-2 w-full">
               <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                Button
+                submit
               </button>
             </div>
             <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
